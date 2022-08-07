18fb5d7f2a7bae9d68059e56960338bfbc5f131f
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2022-08-07 15:02:19 +0800

    Added frontend folder and inital dashboard and login pages

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Pages
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+// Componenets
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={ <Dashboard/> } />
+            <Route path='/login' element={ <Login/> } />
+            <Route path='/register' element={ <Register/> } />
+          </Routes>
+        </div>
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
